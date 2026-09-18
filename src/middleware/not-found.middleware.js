3f6b5cf8e5d5030ef6c93c2f474c0dcefaeb7ccf
Alan <alan5520055@gmail.com>
2026-09-18 21:31:00 +0200

    Delete src directory

--- src/middleware/not-found.middleware.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const HttpError = require("../classes/HttpError");
-
-module.exports = (request, response, next) => {
-  next(new HttpError("Not Found", 404));
-};
